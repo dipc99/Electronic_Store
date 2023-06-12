9738f733050ef2a2d31de7e7db50cae2e2c4d0e4
dipc99 <dipalichaudhari99@gmail.com>
2023-06-12 13:46:36 +0530

    Category Entity Done

---
+++ src/main/java/com/lcwt/electronic/store/entities/Category.java
@@ -0,0 +1,30 @@
+package com.lcwt.electronic.store.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @Column(name = "id")
+    private Long categoryId;
+    @Column(name = "category_title", length = 60)
+    private String title;
+    @Column(name = "category_desc", length = 50)
+    private String description;
+
+    private String coverImage;
+
+}
