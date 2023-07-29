e7413839b791e521925f55bc922450a0653a5dd7
dipc99 <dipalichaudhari99@gmail.com>
2023-07-29 23:24:12 +0530

    Annotation added

--- src/main/java/com/lcwt/electronic/store/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/lcwt/electronic/store/exceptions/ResourceNotFoundException.java
@@ -1,10 +1,13 @@
 package com.lcwt.electronic.store.exceptions;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ResourceNotFoundException extends RuntimeException{
 
     String resourceName;
