2435e5de47a25032f7de13ed1870e7e4e65812f1
dipc99 <dipalichaudhari99@gmail.com>
2023-08-01 15:44:21 +0530

    BadApiRequest Exception

---
+++ src/main/java/com/lcwt/electronic/store/exceptions/BadApiRequest.java
@@ -0,0 +1,14 @@
+package com.lcwt.electronic.store.exceptions;
+
+public class BadApiRequest extends RuntimeException{
+
+    public BadApiRequest(String message){
+
+        super(message);
+    }
+    public BadApiRequest(){
+
+        super("Bad Request !!");
+    }
+
+}
